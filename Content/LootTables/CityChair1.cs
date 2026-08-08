8ced079e0eef94442b30d3b1d24a6b5e55aa01d1
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-08 12:30:15 +0800

    Initialize Commit

---
+++ Content/LootTables/CityChair1.cs
@@ -0,0 +1,25 @@
+using ingot.Core.Behaviour.Loot;
+using ingot.Core.Common;
+
+namespace MyAddon.Content.LootTables;
+
+public class CompactDirtLoot : LootTable
+{
+    public override Identifier Identifier => "pafa_city:city_chair1";
+    public override LootTableCategory Category => LootTableCategory.Blocks;
+    public override LootPool[] Pools =>
+    [
+        new()
+        {
+            Rolls = 1,
+            Entries =
+            [
+                new ItemLootEntry("pafa_city:city_chair1")
+                {
+                    Weight = 1,
+                    Functions = [new SetCount { Count = new IntRange(1, 2) }]
+                }
+            ]
+        }
+    ];
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ingot.Core;
+using PropsAndFurnituresAddOnCity.Content.Blocks;
+
+const string packUuid = "77f1fef2-bb39-411a-b25c-ae475c21169f";
+string dataDir = Path.Combine(AppContext.BaseDirectory, "Data");
+
+Pack pack = Pack.Create(packUuid, "Props And Furnitures Add-On City", "Addon Created By RMPlaysMCYT, Created using Ingot by Pyroboost")
+    .AddBlock<CityChair1>();              // textures declared on the block class
+    // .AddItem<MyItem>()                // icon path declared on the item class
+    // .AddEntity<MyEntity>()
+    // .AddClientEntity<MyClientEntity>(); // materials / textures / geometry short-names
+
+pack.PackIcon = Path.Combine(dataDir, "pack_icon.png");
+
+pack.Compile("./output");
