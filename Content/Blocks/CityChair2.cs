613df59d12b21ff62acc42191edba6142b87c1b8
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-21 17:05:21 +0800

    add city char 2

---
+++ Content/Blocks/CityChair2.cs
@@ -0,0 +1,20 @@
+using ingot.Core.Behaviour.Block;
+using ingot.Core.Common;
+
+namespace PropsAndFurnituresAddOnCity.Content.Blocks;
+
+public class CityChair2 : Block
+{
+    public override Identifier Identifier => "pafa_city:city_chair2";
+    public override string? DisplayName => "City Chair 2";
+    public override string? ResourceTexture => "pafa_city_city_chair2";
+    public override string? Sound => "dirt";
+    public override Enums.CatalogueCategory Category => Enums.CatalogueCategory.Construction;
+    public override MaterialInstances MaterialInstances => new()
+    {
+        All = new MaterialInstance(
+            "pafa_city_city_chair1",
+            MaterialInstance.RenderMethods.Opaque,
+            Path.Combine(AppContext.BaseDirectory, "Data", "compact_dirt.png"))
+    };
+}
