56df6a35345b6d9273b22ca53f25fe802c9e219b
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-08-31 18:10:37 +0800

    fix on the server entity

--- Content/Entities/HospitalBed.cs
+++ Content/Entities/HospitalBed.cs
@@ -1,16 +1,19 @@
+using ingot.Core;
 using ingot.Core.Behaviour.Entity;
 using ingot.Core.Common;
+using ingot.Core.Resource;
+using ingot.Core.TraitSystem.Traits.Entity;
 
 public class HospitalBed : Entity
 {
     public override Identifier Identifier => new("pafa_city:hospital_bed");
     public override bool IsSpawnable => true;
     public override bool IsSummonable => true;
-    public override Dictionary<Identifier, IEntityProperty> Scripts => new()
+    public override ServerEntityScripts? Scripts => new()
     {
-        [new ("animate")] = new Scripts
-        {
-            Values = ["ride"]
-        }
-    }
+        Animate =
+        [
+            "ride"
+        ]
+    };
 }
