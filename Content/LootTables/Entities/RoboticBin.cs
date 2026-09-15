3e50479397f2019029060a2c8a4e8b5708a55798
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-09-15 08:45:53 +0800

    Add robotic bin loot table

---
+++ Content/LootTables/Entities/RoboticBin.cs
@@ -0,0 +1,25 @@
+using ingot.Core.Behaviour.Loot;
+using ingot.Core.Common;
+
+namespace MyAddon.Content.LootTables;
+
+public class RoboticBinLoot : LootTable
+{
+    public override Identifier Identifier => "pafa_city:robotic_bin";
+    public override LootTableCategory Category => LootTableCategory.Blocks;
+    public override LootPool[] Pools =>
+    [
+        new()
+        {
+            Rolls = 1,
+            Entries =
+            [
+                new ItemLootEntry("pafa_city:robotic_bin_spawn_egg")
+                {
+                    Weight = 1,
+                    Functions = [new SetCount { Count = new IntRange(1, 2) }]
+                }
+            ]
+        }
+    ];
+}
