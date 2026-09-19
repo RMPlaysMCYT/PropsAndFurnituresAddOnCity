4944607104083cbaeb371f5c51cc5138060920ce
RMPlaysMC YT <ronnel.mitra.77@gmail.com>
2026-09-20 06:01:47 +0800

    changes

--- Content/Blocks/CityChair3.cs
+++ Content/Blocks/CityChair3.cs
@@ -3,11 +3,11 @@ using ingot.Core.Common;
 
 namespace PropsAndFurnituresAddOnCity.Content.Blocks;
 
-public class CityChair2 : Block
+public class CityChair3 : Block
 {
-    public override Identifier Identifier => "pafa_city:city_chair2";
-    public override string? DisplayName => "City Chair 2";
-    public override string? ResourceTexture => "pafa_city_city_chair2";
+    public override Identifier Identifier => "pafa_city:city_chair3";
+    public override string? DisplayName => "City Chair 3";
+    public override string? ResourceTexture => "pafa_city_city_chair3";
     public override string? Sound => "wood";
     public override Enums.CatalogueCategory Category => Enums.CatalogueCategory.Construction;
     public override MaterialInstances MaterialInstances => new()
